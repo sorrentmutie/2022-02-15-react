dbf5f262404734ef8bab5e42e6000305f09eb63d
salvatore <salvatore.sorrentino@live.com>
2022-03-03 19:12:44 +0100

    componenti tipizzati

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 interface Person {
@@ -7,18 +7,29 @@ interface Person {
   surname: string,
   age: number
 }
+interface ProductProps {
+  value: number;
+}
+
+export const Products = (props: ProductProps) =>  <h3>{props.value} prodotti</h3>
+export const Empty = () => <h3>Non ci sono prodotti</h3>
+
+const RenderMessage = (props: any) => {
+  return props.products === 0 ? <Empty/> : <Products value = {props.products}/>
+}
 
 
 function App() {
   let person: Person | null =
     { name: 'Mario', surname: 'Rossi', age: 50};
   person = null;
+  const products = 5;
   let testo: string = person && person?.['name'] ?  person?.['name'] : "Non esiste";
-
   return (
     <div className="App">
       <header className="App-header">
        <h2>{testo}</h2>
+       <RenderMessage products={products}/>
       </header>
     </div>
   );
