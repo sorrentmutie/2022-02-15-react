696e2eb6fdcc000d3eb9b6c631d8c1b13510d1de
salvatore <salvatore.sorrentino@live.com>
2022-03-03 19:35:25 +0100

    css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Products2 } from './components/Product';
 import { RenderMessage } from './components/RenderMessage';
 import { Person } from './models/person';
@@ -10,11 +11,19 @@ function App() {
     { name: 'Mario', surname: 'Rossi', age: 50};
   person = null;
   const products = 5;
+  const gender: string = 'F';
+  const wrapperClass: string = 
+    gender === 'M' ? 'App-male' : 'App-female';
+
   let testo: string = person && person?.['name'] ?  person?.['name'] : "Non esiste";
   return (
     <div className="App">
       <header className="App-header">
        <h2>{testo}</h2>
+       <div className={wrapperClass}>
+         You are {gender === 'M' ? 'male': 'female'}
+       </div>
+       <div className="badge bg-primary" style={ {color: 'white'}}>Prova</div>
        <RenderMessage products={products}/>
        <Products2 value={products}/>
       </header>
